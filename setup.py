8467b9f815f81265025af428551d7e1f949605db
dafg05 <danialefloresg@gmail.com>
2024-01-14 18:39:55 -0500

    upped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hvo_processing',
-    version='0.1.0',
+    version='0.1.1',
     author='Daniel Flores Garcia',
     author_email='danialefloresg@gmail.com',
     description='Used for drum gen project.',

8f9ed19fb8e093453deaf4297aaef62ef6cfc400
dafg05 <danialefloresg@gmail.com>
2024-01-14 15:01:32 -0500

    change project structure for packaging

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='hvo_processing',
     version='0.1.0',
     author='Daniel Flores Garcia',
-    author_email='danialefloresg@gmail.com',  # Replace with the original author's email
-    description='Used for drum gen project.',  # Provide a short description
+    author_email='danialefloresg@gmail.com',
+    description='Used for drum gen project.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/dafg05/processing',  # Replace with your fork's URL
+    url='https://github.com/dafg05/processing',
     packages=find_packages(),
     install_requires=[
         "note_seq",
@@ -16,14 +16,12 @@ setup(
         "mido"
     ],
     classifiers=[
-        # Choose classifiers as appropriate for your project
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  # Change the license if different
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    python_requires='>=3.6',  # Specify compatible Python versions
+    python_requires='>=3.6',
 )
